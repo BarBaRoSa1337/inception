b6872a534872f00bf6a6fe7c9817656eefc8d67a
BarBaRoSa1337 <achakour@student.1337.ma>
2025-09-29 14:44:19 +0100

    hello

--- requirements/mariadb/tools/entrypoint.sh
+++ requirements/mariadb/tools/entrypoint.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-trap "exit" TERM
- 
+set -e
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql > /dev/null 2>&1
-    sleep 3
+    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 fi
 
-service mariadb start;
+service mariadb start
 sleep 3
-mysql -e "CREATE DATABASE IF NOT EXISTS `${SQL_DATABASE}`;"
-mysql -e "CREATE USER IF NOT EXISTS `${SQL_USER}`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON `${SQL_DATABASE}`.* TO `${SQL_USER}`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+# mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'localhost';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
 mysql -e "FLUSH PRIVILEGES;"
-service mariadb stop;
+
+service mariadb stop
 
 exec mysqld
